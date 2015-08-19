d2f67fab71fd6cfb238171ab425f946d51811a4b
camilo9020 <camilo9020@gmail.com>
2015-08-19 14:59:53 -0500

    change paths for routes names

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
 
 
-  get 'products', to: 'products#index'
-
+  get 'products', to: 'products#index', as: :products
+  get 'products/new', to: 'products#new', as: :new_product
   post 'products', to: 'products#create'
-
-  get 'products/new', to: 'products#new'
-
   get 'products/:id', to: 'products#show'
-
   get 'products/:id/edit', to: 'products#edit'
-
   patch 'products/:id', to: 'products#update'
-
   get '/welcome', to: 'welcome#index'
 
 
