390cb5fd183db19955e72598172142b7d586c5e7
camilo9020 <camilo9020@gmail.com>
2015-09-17 16:21:20 -0500

    logout users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-  user = User.find_by(email: params[:email])
-  if user && user.authenticate(params[:password])
-    sign_in(user)
-    redirect_to root_path
-  else
-    render :new
+	  user = User.find_by(email: params[:email])
+	  if user && user.authenticate(params[:password])
+	    sign_in(user)
+	    redirect_to root_path
+	  else
+	    render :new
+	  end
   end
- end
+
+  def destroy
+  	sign_out
+  	redirect_to root_path
+  	
+  end
 
 
 end
