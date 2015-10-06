d947cefea0d5ea63de8f2bbfcdd8dcfc5571261d
camilo9020 <camilo9020@gmail.com>
2015-10-06 12:10:46 -0500

    Change language of error messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
 
   root 'products#index'
-
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
   resources :users, only:[:new, :create]
   resources :products
- 
-
- 
   
 end
